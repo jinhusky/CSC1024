c3c4b6010324cfc2dd2d53d85797fe7e65cb170c
jinhusky <123972541+jinhusky@users.noreply.github.com>
2023-12-03 14:29:45 +0000

    assignment.py

--- assignment.py
+++ assignment.py
@@ -9,28 +9,52 @@ import os
 import datetime
 
 def main():
+    show_menu()
+    options_menu = get_int_menu("Select option: ")
 
-    options_menu = get_int("Select option: ")
+    books = []
+
+    with open("books.txt") as txtfile:
+        for line in txtfile:
+            ISBN, author, title, publisher, genre, year_published, date_purchased, status = line.rstrip().split(",")
+            book = {"ISBN": ISBN, "author": author, "title": title, "publisher": publisher
+                          , "genre": genre, "year_published": year_published, "date_purchased": date_purchased, "status": status}
+            books.append(book)
+            
+    print(books)
+
+    
 
 
 
 
-def get_int(num):
+"""
+clear screen
+    os.system(f"ipconfig /all") 
+"""
+    
+def add_book(database):
+    ...
+    
+
+
+def get_int_menu(num):
     while True:
         try:
             option = int(input(num))
+            if option < 1 or option > 6: 
+                print("Invalid input please enter 1 to 6")
+                continue
         
         except ValueError:
-            print("Please enter a number !")
+            print("Invalid input, please enter a number!")
             continue
     
         return option
 
 def show_menu():
-    print(f"[1] Add Book Record(s)\n [2] Update/Edit Book Record(s)\n [3] Update/Edit Book Record(s) [4] Display [5] Search [6] Exit")
+    print(f"[1] Add Book Record(s)\n[2] Update/Edit Book Record(s)\n[3] Update/Edit Book Record(s)\n[4] Display\n[5] Search\n[6] Exit")
           
 
-
-    
 if __name__ == "__main__":
     main()
